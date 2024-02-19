b30ad71b5babe1995e0d69591d3e3c081ce38221
willyc2004 <133841163+willyc2004@users.noreply.github.com>
2024-02-19 20:40:24 +0700

    Initial Commit

---
+++ DrawBatik/DrawBatikApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawBatikApp.swift
+//  DrawBatik
+//
+//  Created by MacBook Pro on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DrawBatikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
